28b72e3b0af0b739757a1da6b8739be94e8a87a0
GrzegorzWoj <gregorz.woj@gmail.com>
2023-04-16 11:45:44 +0200

    First commit.

---
+++ LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/LegoInvestorManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.legoinvestormanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoInvestorManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegoInvestorManagerApplication.class, args);
+    }
+
+}
---
+++ LEGO-Investor-Manager/src/test/java/pl/coderslab/legoinvestormanager/LegoInvestorManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.legoinvestormanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoInvestorManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
