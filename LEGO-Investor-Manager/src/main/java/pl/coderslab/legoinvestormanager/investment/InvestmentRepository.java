f1515d084fc88463d05c4bb9c4a9db08f7d6f23a
GrzegorzWoj <gregorz.woj@gmail.com>
2023-04-24 19:16:44 +0200

    Updating current price of LEGO sets. Additional read methods for Portfolios and Investments.

--- LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/investment/InvestmentRepository.java
+++ LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/investment/InvestmentRepository.java
@@ -2,6 +2,10 @@ package pl.coderslab.legoinvestormanager.investment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
+
+    List<Investment> findAllByPortfolioId(Long id);
 
 }
--- LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/legoSet/LegoSetService.java
+++ LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/legoSet/LegoSetService.java
@@ -1,8 +1,14 @@
 package pl.coderslab.legoinvestormanager.legoSet;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LegoSetService {
@@ -38,4 +44,29 @@ public class LegoSetService {
     public void delete(Long id) {
         repository.deleteById(id);
     }
+
+    public List<LegoSetDTO> readAll() {
+        return repository.findAll().stream()
+                .map(mapper::mapToLegoSetDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void updateCurrentPrice(Long id) {
+        LegoSet legoSet = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("LegoSet not found"));
+        try {
+            Document doc = Jsoup.connect("https://promoklocki.pl/" + legoSet.getLegoSetNumber())
+                    .userAgent("Mozilla" /* /5.0 (X11; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0"*/)
+                    .get();
+            String price = doc.select("div.col-md-6 dd a.bprice").text();
+            legoSet.setLowestCurrentPrice(Double.parseDouble(price.substring(0, price.length()-2).replace(',', '.')));
+            repository.save(legoSet);
+        } catch (IOException e) {
+            System.out.println("******************** Nie znaleziono źródła ceny *******************");
+//            throw new RuntimeException(e);
+        }
+    }
+
+
 }
+
