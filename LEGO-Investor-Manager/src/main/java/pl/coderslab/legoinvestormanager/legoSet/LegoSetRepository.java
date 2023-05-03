fbdad00724d220c833f1a1bcb13ad2cb3bf29517
GrzegorzWoj <gregorz.woj@gmail.com>
2023-05-03 13:04:27 +0200

    Improvement of price updating methods.

--- LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/legoSet/LegoSetRepository.java
+++ LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/legoSet/LegoSetRepository.java
@@ -2,6 +2,8 @@ package pl.coderslab.legoinvestormanager.legoSet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LegoSetRepository extends JpaRepository<LegoSet, Long> {
 
 }
