2c63bef29c88b2521cb945ae68fd1cd9b46af71b
GrzegorzWoj <gregorz.woj@gmail.com>
2023-05-10 13:29:15 +0200

    Minor changes in tests classes.

--- LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/legoSet/LegoSet.java
+++ LEGO-Investor-Manager/src/main/java/pl/coderslab/legoinvestormanager/legoSet/LegoSet.java
@@ -2,7 +2,6 @@ package pl.coderslab.legoinvestormanager.legoSet;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import javax.persistence.*;
 
